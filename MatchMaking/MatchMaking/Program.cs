691d73dcd38d4e4481368352b3f3f52c41d64abe
Lokhajtasosmokus <37802979+Lokhajtasosmokus@users.noreply.github.com>
2018-09-10 17:33:08 +0200

    Add files via upload

---
+++ MatchMaking/MatchMaking/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Matchmaking
+{
+    public class Matchmaking
+    {
+        public static void Main(string[] args)
+        {
+            var girls = new List<string> { "Eve", "Ashley", "Bözsi", "Kat", "Jane" };
+            var boys = new List<string> { "Joe", "Fred", "Béla", "Todd", "Neef", "Jeff" };
+
+            // Write a method that joins the two lists by matching one girl with one boy into a new list
+            // Exepected output: "Eve", "Joe", "Ashley", "Fred"...
+
+            
+            Console.WriteLine(MakingMatches(girls, boys));
+        }
+
+        public static string MakingMatches(List<string> girls, List<string> boys)
+        
+        {
+            string together = ""; 
+        }
+        
+        for (int i = 0; i< girls.count; i++)
+			{
+
+			}
+        
+        //az új string, amibe össze kell tenni a 2 listát 
+
+
+        // for ciklusban : mindegyik listáról 1-1 elemet belepakolni a together stringbe 
+        // return a together nevű string 
+
+
+
+    }
+}
