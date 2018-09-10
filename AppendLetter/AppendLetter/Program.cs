370ff5a33573c42f8c61a203489c2172d531ba03
Lokhajtasosmokus <37802979+Lokhajtasosmokus@users.noreply.github.com>
2018-09-10 17:03:27 +0200

    Add files via upload

---
+++ AppendLetter/AppendLetter/Program.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppendLetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {          
+            {
+                var far = new List<string> { "kuty", "macsk", "kacs", "rók", "halacsk" };
+                // Create a method called AppendA() that adds "a" to every string in the far list.
+                // The parameter should be a list.
+
+                Console.WriteLine(AppendA(far));
+                // Expected output: "kutya", "macska", "kacsa", "róka", "halacska"
+            }
+
+            public static string 
+
+
+
+        }
+                       
+    }
+}
