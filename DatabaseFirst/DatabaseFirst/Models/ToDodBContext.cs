4e6192d323a9bc4d9c19982c312c7a2dcaec14f4
Lokhajtasosmokus <37802979+Lokhajtasosmokus@users.noreply.github.com>
2018-09-10 17:20:42 +0200

    Add files via upload

---
+++ DatabaseFirst/DatabaseFirst/Models/ToDodBContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DatabaseFirst.Models
+{
+    public partial class ToDodBContext : DbContext
+    {
+        public ToDodBContext()
+        {
+        }
+
+        public ToDodBContext(DbContextOptions<ToDodBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<ToDos> ToDos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ToDodB;Integrated Security=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {}
+    }
+}
---
+++ DatabaseFirst/DatabaseFirst/Models/ToDos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DatabaseFirst.Models
+{
+    public partial class ToDos
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDone { get; set; }
+        public bool IsUrgent { get; set; }
+    }
+}
